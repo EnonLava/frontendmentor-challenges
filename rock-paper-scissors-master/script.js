b2faf083d348a87865894da9fcd61ba0f19dd59f
Lava519 <lavaboi14@gmail.com>
2023-10-09 23:27:45 +0200

    Finished rough game logic

--- rock-paper-scissors-master/script.js
+++ rock-paper-scissors-master/script.js
@@ -1,31 +1,54 @@
 const values = [ "rock", "scissors", "paper"];
+let score = document.getElementById("score");
 
 function game(player, com) {
-    console.log(player);
-    console.log(com);
+    let result = document.getElementById("result");
+    let resultContainer = document.getElementsByClassName("result-container")[0]
     if (player == com)
-        console.log("tie");
+        result.innerText = "TIE";
     else if ( player == "paper" && com == "rock")
-        console.log("win");
+        result.innerText = "YOU WIN";
     else if ( player == "scissors" && com == "paper" )
-        console.log("win");
+        result.innerText = "YOU WIN";
     else if ( player == "rock" && com == "scissors" )
-        console.log("win");
-    else
-        console.log("lose");
+        result.innerText = "YOU WIN";
+    else {
+        result.innerText = "YOU LOSE";
+        if (score.innerText > 0)
+            score.innerText--;
+    }
+
+    if (result.innerText == "YOU WIN")
+        score.innerText++;
+        
+    resultContainer.style.display = "flex";
+}
+
+function reset() {
+    let pickState = document.getElementsByClassName("state-1")[0];
+    let resultState = document.getElementsByClassName("state-result")[0];
+    let resultContainer = document.getElementsByClassName("result-container")[0]
+    let coins = document.getElementsByClassName("coin-slot");
+    for (let i = 0; i < coins.length; ++i)
+        coins[i].removeChild(coins[i].firstElementChild);
+    pickState.style.display = "block";
+    resultState.style.display = "none";
+    resultContainer.style.display = "none";
 }
 
 function pick(value) {
     let pickState = document.getElementsByClassName("state-1")[0];
-    let resultState = document.getElementsByClassName("state-2")[0];
+    let resultState = document.getElementsByClassName("state-result")[0];
     pickState.style.display = "none";
-    resultState.style.display = "flex";
+    resultState.style.display = "block";
     let coin = document.getElementsByClassName(value)[0];
     let coinSlot = document.getElementsByClassName("coin-slot");
     let comValue = values[Math.floor(Math.random() * 3)];
     let com = document.getElementsByClassName(comValue)[0];
     coinSlot[0].appendChild(coin.cloneNode(true));
     coinSlot[1].appendChild(com.cloneNode(true));
+    coinSlot[0].children[0].onclick = null;
+    coinSlot[1].children[0].onclick = null;
     game(value, comValue);
 
 }
