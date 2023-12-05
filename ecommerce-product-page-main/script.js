a7c0a0cfb1d702e6c69fb8268ce061093465fa5e
Lava519 <lavaboi14@gmail.com>
2023-12-05 02:18:37 +0100

    Cycle mobile images

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,20 +1,37 @@
-const cartIncrease = document.getElementById("increase");
-const cartDecrease = document.getElementById("decrease");
 const cartDisplayValue = document.getElementById("cart-value")
-
+document.getElementById("increase").addEventListener("click", ()=>{cartValue(true)});
+document.getElementById("decrease").addEventListener("click", ()=>{cartValue(false)});
 let cartDisplay = 0;
 
-cartIncrease.addEventListener("click", ()=>{cartValue(true)});
-cartDecrease.addEventListener("click", ()=>{cartValue(false)});
+const mobileImage = document.getElementById("mobile-image")
+document.getElementById("next").addEventListener("click", ()=>{nextImage()})
+document.getElementById("previous").addEventListener("click", ()=>{previousImage()})
+const images = 4;
+let imgIndex = 1;
+
+function nextImage() {
+    let url = mobileImage.src;
+    ++imgIndex;
+    if (imgIndex > images)
+        imgIndex = 1;
+    mobileImage.src = url.slice(0,-5) + imgIndex + url.slice(-4);
+}
+
+function previousImage() {
+    let url = mobileImage.src;
+    --imgIndex;
+    if (imgIndex < 1)
+        imgIndex = images;
+    mobileImage.src = url.slice(0,-5) + imgIndex + url.slice(-4);
+}
 
 function cartValue(increase) {
-    cartDisplayValue.innerText = cartDisplay;
     if (increase) {
-        if (cartDisplayValue.innerText < 100)
-            cartDisplayValue.innerText++;
+        if (cartDisplay < 100)
+            cartDisplay++;
     }else {
-        if (cartDisplayValue.innerText > 0)
-            cartDisplayValue.innerText--;
+        if (cartDisplay > 0)
+            cartDisplay--;
     }
-    cartDisplay = cartDisplayValue.innerText
+    cartDisplayValue.innerText = cartDisplay;
 }
